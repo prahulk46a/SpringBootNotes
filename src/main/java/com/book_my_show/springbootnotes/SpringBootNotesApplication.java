7cb847d5228216e97ac43be632656b45992070e9
prahulk46a <prahulk212@gmail.com>
2024-12-05 20:50:02 +0530

    First Commit

---
+++ src/main/java/com/book_my_show/springbootnotes/SpringBootNotesApplication.java
@@ -0,0 +1,13 @@
+package com.book_my_show.springbootnotes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootNotesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootNotesApplication.class, args);
+    }
+
+}
